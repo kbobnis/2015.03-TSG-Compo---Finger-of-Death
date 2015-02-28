037f1ec2431b6583d03ee7be121d45501e74be18
Tomasz Trzaska <tommy.trzaska@gmail.com>
2015-02-28 16:31:03 +0100

    Ludziki

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,14 +5,19 @@ using System.Collections.Generic;
 public class Game : MonoBehaviour {
 
 	public GameObject PanelMinigame;
+	public GameObject PanelPeople;
+	public GameObject PanelTiles;
+	public static Game Me;
 
 	void Awake() {
+		Me = this;
 		List<TileType> tiles = new List<TileType>();
 
 		for (int i = 0; i < 35; i++) {
 			tiles.Add(TileType.GetRandom());
 		}
 		PanelMinigame.GetComponent<PanelMinigame>().Prepare(tiles);
+		PanelPeople.GetComponent<PanelPeople> ().SpawnPeople ();
 	}
 	
 }
---
+++ Assets/Scripts/Panels/PanelPeople.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PanelPeople : MonoBehaviour {
+
+	public GameObject PersonPrefab;
+
+	internal void SpawnPeople (){
+		GameObject[] people = new GameObject[4];
+
+		for (int i=0; i<4; i++) {
+			people [i] = Instantiate (PersonPrefab) as GameObject;
+			people [i].transform.parent = transform;
+		}
+
+		people [0].GetComponent<InGamePos> ().Set (0, 0);
+		people [1].GetComponent<InGamePos> ().Set (4, 0);
+		people [2].GetComponent<InGamePos> ().Set (0, 6);
+		people [3].GetComponent<InGamePos> ().Set (4, 6);
+	}
+}
