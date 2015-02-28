f4c65c530e88e9a2fb7ac00988a3302ccfcea4ad
Tomasz Trzaska <tommy.trzaska@gmail.com>
2015-02-28 14:15:56 +0100

    GUI

---
+++ Assets/Scripts/Panels/PanelGUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PanelGUI : MonoBehaviour {
+
+	public Text Timer;
+
+	private int seconds = 0;
+
+	void Start () {
+		StartCoroutine ("TimeCount");
+	}
+
+	IEnumerator TimeCount (){
+		while (true) {
+			yield return new WaitForSeconds (1);
+			seconds++;
+			Timer.text = seconds.ToString ();
+		}
+	}
+}
