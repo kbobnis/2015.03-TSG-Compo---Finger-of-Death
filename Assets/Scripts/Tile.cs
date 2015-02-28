955ac288545b288b48209d81f40feecad029916c
Tomasz Trzaska <tommy.trzaska@gmail.com>
2015-02-28 18:55:08 +0100

    Rotation of tiles

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,13 +9,48 @@ public class Tile : MonoBehaviour{
 
 	public GameObject GameObjectImage;
 
+	private TileTemplate template;
 
 	public void Prepare(TileTemplate tt) {
 		GameObjectImage.GetComponent<Image>().sprite = tt.TileType.Image;
 		//rotation
 		GameObjectImage.transform.Rotate(0, 0, tt.Rotation.Value);
+
+		template = tt;
 	}
 
 	void Update() {
+		switch(template.TileType.Id){
+			case "Side":
+			case "Slant":{
+				ChangeVersion ();
+				break;
+			}
+			case "Cross":
+			case "Edge": break;
+			default:{
+				Debug.Log ("Undefined type");
+				break;
+			}
+		}
+	}
+
+	void ChangeVersion (){
+		switch (template._Version){
+			case Version._1: {
+				GameObjectImage.transform.Rotate (180,0,0);
+				template._Version = Version._2;
+				return;
+			}
+			case Version._2: {
+				GameObjectImage.transform.Rotate (180,0,0);
+				template._Version = Version._1;
+				return;
+			}
+			default:{
+				Debug.Log ("Undefined version");
+				return;
+			}
+		}
 	}
 }
