7749bfefebd2e37555e25be87459c43a1490bbef
Maciek Golec <necromac.3@gmail.com>
2015-03-01 20:23:49 +0100

    Panel Fixes

--- Assets/Scripts/Panels/PanelBonuses.cs
+++ Assets/Scripts/Panels/PanelBonuses.cs
@@ -9,33 +9,35 @@ public class PanelBonuses : MonoBehaviour {
 	internal void Prepare (){
 		StartCoroutine (SpawnBuff ());
 	}
+	IEnumerator SpawnBuff()
+	{
+		while (true)
+		{
+			GameObject buffGameObject = Instantiate(PersonPrefab) as GameObject;
 
-	IEnumerator SpawnBuff (){
-		while (true) {
-			GameObject buffGameObject = Instantiate (PersonPrefab) as GameObject;
-			
-			buffGameObject.SetActive (true);
-			Person p = buffGameObject.GetComponent<Person> ();
+			buffGameObject.SetActive(true);
+			Person p = buffGameObject.GetComponent<Person>();
 			buffGameObject.transform.parent = transform;
 			buffGameObject.name = "buff";
-			float posX = Random.Range (0, 5) + 0.5f;
-			float posY = Random.Range (0, 6) + 0.5f;
-			
-			buffGameObject.AddComponent<Buff> ();
-			p.ImageCone.SetActive (false);
-			p.Prepare ((int)posX, (int)posY);
-			p.SetStats (1, 0, 0, Person.CollisionGroup.Chest);
-			p.ImageAvatar.GetComponent<Image> ().sprite = SpriteManager.BuffSprite;
+			float posX = Random.Range(0, 5) + 0.5f;
+			float posY = Random.Range(0, 6) + 0.5f;
+
+			p.ImageAvatar.AddComponent<TouchToKill>().Prepare();
+			buffGameObject.AddComponent<Buff>();
+			p.ImageCone.SetActive(false);
+			p.Prepare((int)posX, (int)posY);
+			p.SetStats(1, 0, 0, Person.CollisionGroup.Chest);
+			p.ImageAvatar.GetComponent<Image>().sprite = SpriteManager.BuffSprite;
 			p.ImageAvatar.AddComponent<PlayerCollisionScript>().SetOwner(p);
-			
+
 			buffGameObject.name = "buff x: " + posX + ", y: " + posY;
-			int buffW = (int)(p.ImageAvatar.GetComponent<Image> ().sprite.rect.width * AspectRatioKeeper.ActualScale);
-			int buffH = (int)(p.ImageAvatar.GetComponent<Image> ().sprite.rect.height * AspectRatioKeeper.ActualScale);
-			buffGameObject.AddComponent<RealSize> ().SetSize (buffW, buffH);
-			buffGameObject.AddComponent<InGamePos> ().UpdatePos (posX, posY);
-			
-			
-			p.ImageAvatar.AddComponent<Rigidbody> ();
+			int buffW = (int)(p.ImageAvatar.GetComponent<Image>().sprite.rect.width * AspectRatioKeeper.ActualScale);
+			int buffH = (int)(p.ImageAvatar.GetComponent<Image>().sprite.rect.height * AspectRatioKeeper.ActualScale);
+			buffGameObject.AddComponent<RealSize>().SetSize(buffW, buffH);
+			buffGameObject.AddComponent<InGamePos>().UpdatePos(posX, posY);
+
+
+			p.ImageAvatar.AddComponent<Rigidbody>();
 			p.ImageAvatar.rigidbody.isKinematic = true;
 			p.ImageAvatar.rigidbody.useGravity = false;
 			p.ImageAvatar.AddComponent<BoxCollider>();
--- Assets/Scripts/Panels/PanelPeople.cs
+++ Assets/Scripts/Panels/PanelPeople.cs
@@ -17,6 +17,7 @@ public class PanelPeople : MonoBehaviour {
 		}
 		SpawnPerson(2, 3, Person.CollisionGroup.Player, Person.CharacterType.Player);
 
+		//StartCoroutine(SpawnBuff());
 		foreach(GameObject go in People){
 			go.GetComponent<Person>().ShadeOfCones = 0;
 		}
@@ -91,5 +92,42 @@ public class PanelPeople : MonoBehaviour {
 			Destroy(People[i]);
 		}
 		People.Clear();
+	}
+	IEnumerator SpawnBuff()
+	{
+		while (true)
+		{
+			GameObject buffGameObject = Instantiate(PersonPrefab) as GameObject;
+
+			buffGameObject.SetActive(true);
+			Person p = buffGameObject.GetComponent<Person>();
+			buffGameObject.transform.parent = transform;
+			buffGameObject.name = "buff";
+			float posX = Random.Range(0, 5) + 0.5f;
+			float posY = Random.Range(0, 6) + 0.5f;
+
+			p.ImageAvatar.AddComponent<TouchToKill>().Prepare();
+			buffGameObject.AddComponent<Buff>();
+			p.ImageCone.SetActive(false);
+			p.Prepare((int)posX, (int)posY);
+			p.SetStats(1, 0, 0, Person.CollisionGroup.Chest);
+			p.ImageAvatar.GetComponent<Image>().sprite = SpriteManager.BuffSprite;
+			p.ImageAvatar.AddComponent<PlayerCollisionScript>().SetOwner(p);
+
+			buffGameObject.name = "buff x: " + posX + ", y: " + posY;
+			int buffW = (int)(p.ImageAvatar.GetComponent<Image>().sprite.rect.width * AspectRatioKeeper.ActualScale);
+			int buffH = (int)(p.ImageAvatar.GetComponent<Image>().sprite.rect.height * AspectRatioKeeper.ActualScale);
+			buffGameObject.AddComponent<RealSize>().SetSize(buffW, buffH);
+			buffGameObject.AddComponent<InGamePos>().UpdatePos(posX, posY);
+
+
+			p.ImageAvatar.AddComponent<Rigidbody>();
+			p.ImageAvatar.rigidbody.isKinematic = true;
+			p.ImageAvatar.rigidbody.useGravity = false;
+			p.ImageAvatar.AddComponent<BoxCollider>();
+			(p.ImageAvatar.collider as BoxCollider).size = new Vector3(16, 16, 16);
+			p.ImageAvatar.collider.isTrigger = true;
+			yield return new WaitForSeconds(5);
+		}
 	}
 }
