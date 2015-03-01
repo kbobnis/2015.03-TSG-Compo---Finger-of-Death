0c8c0cebf78184ed4730019e3be458361f901635
Tomasz Trzaska <tommy.trzaska@gmail.com>
2015-03-01 15:27:52 +0100

    fixed Buff

--- Assets/Scripts/Buff.cs
+++ Assets/Scripts/Buff.cs
@@ -3,28 +3,16 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Buff : MonoBehaviour {
-
-	public float X, Y;
-	public float _Time;
+	
+	public float Time;
 	public float DeltaLife;
 	public float DeltaSpeed;
 	public int DeltaPoints;
 	public bool Undestructable;
 
-	internal void Prepare(int x, int y) {
-		X = x;
-		Y = y;
-
-		Sprite s = Resources.Load<Sprite> ("Images/testBuff");
-		GetComponent<Image>().sprite = s;
-		gameObject.AddComponent<InGamePos>();
-		GetComponent<InGamePos>().UpdatePos(X, Y);
-
-		Debug.Log (X + ", " + Y);
-	}
 
 	public void GainBuff (){
-		Debug.Log ("Gain: time: " + _Time + ", life: " + DeltaLife + ", speed: " + DeltaSpeed + ", points: " + DeltaPoints + ", immortality: " + Undestructable);
+		Debug.Log ("Gain: time: " + Time + ", life: " + DeltaLife + ", speed: " + DeltaSpeed + ", points: " + DeltaPoints + ", immortality: " + Undestructable);
 		Destroy (gameObject);
 	}
 }
--- Assets/Scripts/Panels/PanelPeople.cs
+++ Assets/Scripts/Panels/PanelPeople.cs
@@ -42,15 +42,18 @@ void SpawnBuff (){
 		GameObject buffGameObject = Instantiate (PersonPrefab) as GameObject;
 		buffGameObject.transform.parent = transform;
 		buffGameObject.name = "buff";
-		int posX = Random.Range (0, 7);
-		int posY = Random.Range (0, 5);
+		float posX = Random.Range (0, 5)+0.5f;
+		float posY = Random.Range (0, 7)+0.5f;
+
+		buffGameObject.GetComponent<Image> ().sprite = SpriteManager.BuffSprite;
+
 		buffGameObject.name = "buff x: "+posX+", y: "+posY;
-		buffGameObject.AddComponent<Buff> ().Prepare (posX, posY);
 		int buffW = (int)(buffGameObject.GetComponent<Image>().sprite.rect.width * AspectRatioKeeper.ActualScale);
 		int buffH = (int)(buffGameObject.GetComponent<Image>().sprite.rect.height * AspectRatioKeeper.ActualScale);
+		buffGameObject.AddComponent<RealSize>().SetSize(buffW, buffH);
+		buffGameObject.AddComponent<InGamePos>().UpdatePos(posX, posY);
 		buffGameObject.AddComponent<BoxCollider>();
 		(buffGameObject.collider as BoxCollider).size = new Vector3(16, 16, 16);
 		buffGameObject.collider.isTrigger = true;
-		buffGameObject.AddComponent<RealSize>().SetSize(buffW, buffH);
 	}
 }
